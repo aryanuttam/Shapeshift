6217b09d14df96e97258b187fa4abf093f5d4a05
aryanuttam <aryanuttam786@gmail.com>
2023-11-23 14:03:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+
+const express = require("express");
+const app=express();
+const path=require('path');
+
+app.use(express.urlencoded({extended:false}))
+
+app.use(express.static(path.join(__dirname,'public')))
+app.set("view engine",'ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+app.get('/index',(req,res)=>{
+    res.render('index')
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about')
+})
+
+app.get('/blog',(req,res)=>{
+    res.render('blog')
+})
+app.get('/contact',(req,res)=>{
+    res.render('contact')
+})
+app.get('/gallery',(req,res)=>{
+    res.render('gallery')
+})
+
+app.get('/weightlifting',(req,res)=>{
+    res.render('weightlifting')
+})
+
+app.get('/running',(req,res)=>{
+    res.render('running')
+})
+
+app.get('/yoga',(req,res)=>{
+    res.render('yoga')
+})
+
+
+app.listen(3000,()=>{
+    console.log('connect to server');
+})
